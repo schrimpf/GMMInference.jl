4e185119d53ad05320342c5b0cfb8a5c49f9292d
Paul Schrimpf <paul.schrimpf@gmail.com>
2019-10-20 09:54:25 -0700

    Initial commit
    Added weave file.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # GMMInference
 
-[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://schrimpf.github.io/GMMInference.jl/stable)
+Methods for inference for generalized method of moments and other extremum estimators. 
+
+See [the docs for examples](https://schrimpf.github.io/GMMInference.jl/dev).
+
+<!-- [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://schrimpf.github.io/GMMInference.jl/stable) -->
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://schrimpf.github.io/GMMInference.jl/dev)
-[![Build Status](https://travis-ci.com/schrimpf/GMMInference.jl.svg?branch=master)](https://travis-ci.com/schrimpf/GMMInference.jl)
-[![Codecov](https://codecov.io/gh/schrimpf/GMMInference.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/schrimpf/GMMInference.jl)
+<!-- [![Build Status](https://travis-ci.com/schrimpf/GMMInference.jl.svg?branch=master)](https://travis-ci.com/schrimpf/GMMInference.jl) -->
+<!-- [![Codecov](https://codecov.io/gh/schrimpf/GMMInference.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/schrimpf/GMMInference.jl) -->
--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,53 @@
-using Documenter, GMMInference
+using Documenter, GMMInference, DocumenterMarkdown
 
-makedocs(;
-    modules=[GMMInference],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-    ],
-    repo="https://github.com/schrimpf/GMMInference.jl/blob/{commit}{path}#L{line}",
-    sitename="GMMInference.jl",
-    authors="Paul Schrimpf <paul.schrimpf@gmail.com>",
-    assets=String[],
+runweave=true
+runnotebook=false
+if runweave
+  using Weave
+  wd = pwd()
+  try
+    builddir=joinpath(dirname(Base.pathof(GMMInference)),"..","docs","build")
+    mkpath(builddir)
+    cd(builddir)
+    jmdfiles = filter(x->occursin(".jmd",x), readdir(joinpath("..","jmd")))
+    for f in jmdfiles 
+      weave(joinpath("..","jmd",f),out_path=joinpath("..","build"),
+            cache=:refresh,
+            cache_path=joinpath("..","weavecache"),
+            doctype="github", mod=Main,
+            args=Dict("md" => true))
+      if (runnotebook)
+        notebook(joinpath("..","jmd",f),out_path=joinpath("..","build"),
+                 nbconvert_options="--allow-errors")
+      end
+    end
+  finally 
+    cd(wd)
+  end
+  if (isfile("build/temp.md"))
+    rm("build/temp.md")
+  end
+end
+
+makedocs(
+  modules=[GMMInference],
+  format=Markdown(),
+  pages=[
+    "Home" => "index.md", # this won't get used anyway; we use mkdocs instead for interoperability with weave's markdown output.
+  ],
+  repo="https://github.com/schrimpf/GMMInference.jl/blob/{commit}{path}#L{line}",
+  sitename="GMMInference.jl",
+  authors="Paul Schrimpf <paul.schrimpf@gmail.com>",
+  assets=String[],
 )
 
-deploydocs(;
-    repo="github.com/schrimpf/GMMInference.jl",
-)
+run(`mkdocs build`)
+
+#deploydocs(;
+#    repo="github.com/schrimpf/GMMInference.jl",
+#)
+
+deploy=true
+if deploy || "deploy" in ARGS
+  run(`mkdocs gh-deploy`)
+end
