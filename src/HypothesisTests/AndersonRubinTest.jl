025e58f86b2b12e3e3bf02f2d432fcd5ea96788c
Pooya Rashidi Ravari <44382270+PooyaFa@users.noreply.github.com>
2019-11-04 00:26:12 -0800

    update

--- src/HypothesisTests/AndersonRubinTest.jl
+++ src/HypothesisTests/AndersonRubinTest.jl
@@ -12,19 +12,3 @@ function AR_gmm_obj(gi::Function)
     (size(g)[1]*( m*W*m')[1])
   end
 end
-"""
-    AR_test(n::Integer, β::AbstractVector,
-                      π::AbstractMatrix, ρ)
-
-Returns a function `ar(θ)` where:
-``ar(θ) = n [1/n \\sum g_i(θ)]' \\widehat{Var}(g_i(θ))^{-1}[1/n \\sum g_i(θ)] ``
-
-Or equivalent to:
-``AR_gmm_obj(get_gi(IVLogitShare(n,β0,π0,ρ)))``
-"""
-function AR_test(n::Integer, β::AbstractVector,
-                      π::AbstractMatrix, ρ)
-    ivlogitshare = IVLogitShare(n,β0,π0,ρ)
-    gi = get_gi(ivlogitshare)
-    return AR_gmm_obj(gi)
-end
